7594fa83938090927d5e11574a9840a88e711b04
Coxgood <_email@example.com>
2026-08-25 10:45:51 +0700

    помнняли внешний адрес к бд на локальный

--- fastapi_app.py
+++ fastapi_app.py
@@ -12,7 +12,7 @@ import asyncpg
 
 DB_USER = os.getenv("DB_USER", "postgres")
 DB_PASSWORD = os.getenv("DB_PASSWORD", "GlDxzFUy6V")
-DB_HOST = os.getenv("DB_HOST", "82.146.18.156")
+DB_HOST = os.getenv("DB_HOST", "127.0.0.1")
 DB_PORT = os.getenv("DB_PORT", "5432")
 DB_NAME = os.getenv("DB_NAME", "stroy_net")
 
